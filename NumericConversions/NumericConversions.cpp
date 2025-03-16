0409f23968ab3b54ea4216c4b207fd3a945dab8f
potatoSalad21 <eeperpeepo@gmail.com>
2025-03-16 20:12:33 +0400

    numeric conversions exercise done

--- NumericConversions/NumericConversions.cpp
+++ NumericConversions/NumericConversions.cpp
@@ -9,17 +9,72 @@
 
 #include <iostream>
 #include <string>
-#include "console.h"
+#include "StanfordCPPLib/console.h"
+#include "StanfordCPPLib/simpio.h"
 using namespace std;
 
 /* Function prototypes */
 
 string intToString(int n);
 int stringToInt(string str);
+bool isValid(string str);
 
 /* Main program */
 
 int main() {
-    // [TODO: fill in the code]
+    int num = getInteger("Enter integer: ");
+    std::cout << intToString(num) << std::endl;
+
+    string numStr = getLine("Enter a string: ");
+    std::cout << stringToInt(numStr) << std::endl;
+
     return 0;
+}
+
+// recursively convert integer to string
+string intToString(int n) {
+    /* Base Cases */
+    if (n == 0) return "0";
+    // add negative sign
+    if (n < 0) {
+        return "-" + intToString(-n);
+    }
+    if (n < 10) {
+        return string() + char(n + '0');
+    }
+
+    return intToString(n / 10) + intToString(n % 10);
+}
+
+// validate integer input
+bool isValid(string str) {
+    if (str == "") return false;
+
+    int idx = str[0] == '-' ? 1 : 0;
+    for (int i = idx; i < str.length(); i++) {
+        if (!isdigit(str[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// recursively convert string to VALID integers
+int stringToInt(string str) {
+    // base case
+    if (str == "") return 0;
+
+    // validate string
+    if (!isValid(str)) {
+        std::cout << "Invalid number input" << std::endl;
+        return -1;
+    }
+
+    // check if negative
+    if (str[0] == '-') {
+        return -stringToInt(str.substr(1));
+    }
+
+    return str[str.length() - 1] - '0' + 10 * stringToInt(str.substr(0, str.length()-1));
 }
